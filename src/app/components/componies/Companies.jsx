c5bec9c7c4e8fd6466f24bb4009b0434e666e193
Numaan Ali <alinumaan35@gmail.com>
2025-10-23 00:27:53 -0700

    aaaa

--- src/app/components/componies/Companies.jsx
+++ src/app/components/componies/Companies.jsx
@@ -9,15 +9,6 @@ const Companies = () => {
   if (!componeies)
     return <p className="text-center py-10">No company data found</p>;
 
-  const logos = [
-    componeies.img1,
-    componeies.img2,
-    componeies.img3,
-    componeies.img4,
-    componeies.img5,
-    componeies.img6,
-  ].filter(Boolean);
-
   return (
     <section
       className="w-full py-12 text-center px-6 sm:px-10 lg:px-20 bg-white"
@@ -29,18 +20,76 @@ const Companies = () => {
         </h2>
 
         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-x-10 gap-y-10 items-center justify-items-center transition-all duration-300">
-          {logos.map((logo, index) => (
-            <div
-              key={index}
-              className="flex flex-col items-center justify-center text-center"
-            >
+          {componeies.img1 && (
+            <div className="flex flex-col items-center justify-center text-center">
               <img
-                src={logo}
-                alt={`Company ${index + 1}`}
-                className="object-contain opacity-100 hover:opacity-70 transition-all duration-300 h-[100px] sm:h-[100px] md:h-[110px] lg:h-[130px] xl:h-[140px]"
+                src={componeies.img1}
+                alt="Company 1"
+                className="object-contain opacity-100 hover:opacity-70 transition-all duration-300 
+                   h-[140px] sm:h-[160px] md:h-[180px] lg:h-[200px] xl:h-[220px] 
+                   w-[140px] sm:w-[160px] md:w-[180px] lg:w-[200px] xl:w-[220px]"
               />
             </div>
-          ))}
+          )}
+
+          {componeies.img2 && (
+            <div className="flex flex-col items-center justify-center text-center">
+              <img
+                src={componeies.img2}
+                alt="Company 2"
+                className="object-contain opacity-100 hover:opacity-70 transition-all duration-300 
+                   h-[140px] sm:h-[160px] md:h-[180px] lg:h-[200px] xl:h-[220px] 
+                   w-[140px] sm:w-[160px] md:w-[180px] lg:w-[200px] xl:w-[220px]"
+              />
+            </div>
+          )}
+
+          {componeies.img3 && (
+            <div className="flex flex-col items-center justify-center text-center">
+              <img
+                src={componeies.img3}
+                alt="Company 3"
+                className="object-contain opacity-100 hover:opacity-70 transition-all duration-300 
+                   h-[140px] sm:h-[160px] md:h-[180px] lg:h-[200px] xl:h-[220px] 
+                   w-[140px] sm:w-[160px] md:w-[180px] lg:w-[200px] xl:w-[220px]"
+              />
+            </div>
+          )}
+          {componeies.img4 && (
+            <div className="flex flex-col items-center justify-center text-center">
+              <img
+                src={componeies.img4}
+                alt="Company 4"
+                className="object-contain opacity-100 hover:opacity-70 transition-all duration-300 
+                   h-[140px] sm:h-[160px] md:h-[180px] lg:h-[200px] xl:h-[220px] 
+                   w-[140px] sm:w-[160px] md:w-[180px] lg:w-[200px] xl:w-[220px]"
+              />
+            </div>
+          )}
+
+          {componeies.img5 && (
+            <div className="flex flex-col items-center justify-center text-center">
+              <img
+                src={componeies.img5}
+                alt="Company 5"
+                className="object-contain opacity-100 hover:opacity-70 transition-all duration-300 
+                   h-[140px] sm:h-[160px] md:h-[180px] lg:h-[200px] xl:h-[220px] 
+                   w-[140px] sm:w-[160px] md:w-[180px] lg:w-[200px] xl:w-[220px]"
+              />
+            </div>
+          )}
+
+          {componeies.img6 && (
+            <div className="flex flex-col items-center justify-center text-center">
+              <img
+                src={componeies.img6}
+                alt="Company 6"
+                className="object-contain opacity-100 hover:opacity-70 transition-all duration-300 
+                   h-[140px] sm:h-[160px] md:h-[180px] lg:h-[200px] xl:h-[220px] 
+                   w-[140px] sm:w-[160px] md:w-[180px] lg:w-[200px] xl:w-[220px]"
+              />
+            </div>
+          )}
         </div>
       </div>
     </section>
